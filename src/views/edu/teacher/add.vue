1e2363a1eb5a19917d9fa32ba5bf28a373ea2037
464851295@qq.com <2012.zzl>
2020-08-31 10:38:14 +0800

    完成讲师头像上传

--- src/views/edu/teacher/add.vue
+++ src/views/edu/teacher/add.vue
@@ -23,7 +23,31 @@
       <el-form-item label="讲师简介">
         <el-input v-model="teacher.intro" :rows="10" type="textarea"/>
       </el-form-item>
-      <!-- 讲师头像：TODO -->
+
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+          <!-- 头衔缩略图 -->
+          <pan-thumb :image="teacher.avatar"/>
+          <!-- 文件上传按钮 -->
+          <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+          </el-button>
+          <!--
+      v-show：是否显示上传组件
+      :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+      :url：后台上传的url地址
+      @close：关闭上传组件
+      @crop-upload-success：上传成功后的回调 -->
+          <image-cropper
+            v-show="imagecropperShow"
+            :width="300"
+            :height="300"
+            :key="imagecropperKey"
+            :url="BASE_API+'/eduoss/fileoss'"
+            field="file"
+            @close="close"
+            @crop-upload-success="cropSuccess"/>
+      </el-form-item>
+
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
       </el-form-item>
@@ -33,7 +57,12 @@
 <script>
     //引入调用teacher.js文件
     import teacherApi from '@/api/edu/teacher.js'
+    //引入组件
+    import ImageCropper from '@/components/ImageCropper'
+    import PanThumb from '@/components/PanThumb'
 export default {
+  //声明组件
+  components:{ImageCropper,PanThumb},
     data(){//定义变量和初始值
         return{
             teacher:{
@@ -42,8 +71,14 @@ export default {
                 level: 1,
                 career: '',
                 intro: '',
-                avatar: ''
+                avatar: "https://guliedu--online.oss-cn-beijing.aliyuncs.com/favicon.png"
             },
+            //上传弹框组件是否显示
+            imagecropperShow:false,
+            //上传组件key值
+            imagecropperKey:0,
+            //获取 .env.development中的 VUE_APP_BASE_API 的地址
+            BASE_API:process.env.VUE_APP_BASE_API,
             saveBtnDisabled:false   //保存按钮是否禁用
         }
     },
@@ -57,6 +92,20 @@ export default {
     }
   },
     methods:{
+      close(){
+        
+        //关闭弹窗
+        this.imagecropperShow = false
+        // 上传失败后，重新打开上传组件时初始化组件，否则显示上一次的上传结果
+        this.imagecropperKey = this.imagecropperKey + 1
+      },
+      //上传成功方法
+      cropSuccess(data){
+        this.imagecropperShow = false
+        //上传成功之后图片地址
+        this.teacher.avatar = data.url
+        this.imagecropperKey = this.imagecropperKey + 1
+      },
         init(){
             if (this.$route.params && this.$route.params.id) {
                 const id = this.$route.params.id
